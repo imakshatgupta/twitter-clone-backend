7fc1a88665ee7f03036c3d7ff16c855b0e117850
Akshat Gupta <akshatgtc@gmail.com>
2023-11-14 00:11:45 +0530

    change frontend link

--- index.js
+++ index.js
@@ -104,8 +104,8 @@ async function run() {
               quantity: 1,
             },
           ],
-          success_url: "https://twitter-clone-x.netlify.app/",
-          cancel_url: "https://twitter-clone-x.netlify.app/failure",
+          success_url: "https://twitterclone-x.netlify.app/",
+          cancel_url: "https://twitterclone-x.netlify.app/failure",
         });
         res.json({ url: session.url });
       } catch (error) {
@@ -129,8 +129,8 @@ async function run() {
               quantity: 1,
             },
           ],
-          success_url: "https://twitter-clone-x.netlify.app/",
-          cancel_url: "https://twitter-clone-x.netlify.app/failure",
+          success_url: "https://twitterclone-x.netlify.app/",
+          cancel_url: "https://twitterclone-x.netlify.app/failure",
         });
         res.json({ url: session.url });
       } catch (error) {
@@ -155,8 +155,8 @@ async function run() {
               quantity: 1,
             },
           ],
-          success_url: "https://twitter-clone-x.netlify.app/",
-          cancel_url: "https://twitter-clone-x.netlify.app/failure",
+          success_url: "https://twitterclone-x.netlify.app/",
+          cancel_url: "https://twitterclone-x.netlify.app/failure",
         });
 
         res.json({ url: session.url });
